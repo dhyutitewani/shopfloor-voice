8980c902bfc0cd404bc1ba2030cdd00256515e8c
dhyutitewani <dhyuti07@gmail.com>
2024-09-30 19:29:58 +0530

    redirect to /user

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,3 +1,20 @@
-export default function Home() {
-  return null; // Optionally, you could return a loading indicator or message here
-}
+'use client';
+
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation'; // Use next/navigation for Client Components
+import { useSession } from 'next-auth/react';
+
+const HomePage: React.FC = () => {
+    const { status } = useSession(); // Destructure status from useSession
+    const router = useRouter();
+
+    useEffect(() => {
+      if (status === 'unauthenticated') {
+          router.replace('/users'); 
+      }
+    }, [router, status]); // Ensure status is included in the dependency array
+
+    return null; 
+};
+
+export default HomePage;
