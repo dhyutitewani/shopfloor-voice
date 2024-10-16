2594880e0a26eb2c9dfa5120b776fba146d58659
dhyutitewani <dhyuti07@gmail.com>
2024-10-16 17:29:11 +0530

    updated deployment url

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -8,7 +8,7 @@ import express, { Application, NextFunction, Request, Response } from "express";
 const app: Application = express();
 
 app.use(cors({
-  origin:'https://shopfloor-voice.vercel.app',
+  origin:'https://shopfloor-voice-a5c3.vercel.app',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   credentials: true,
 }));
